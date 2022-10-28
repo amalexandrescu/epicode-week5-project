c749854ce8c9491ed8a9d32e2c03d88e0f45e64d
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-10-28 10:51:58 +0100

    create first version template

---
+++ script.js
@@ -0,0 +1,9 @@
+const addButton = document.querySelector("#add-button");
+
+const plusMinusButton = document.querySelectorAll(".plus-minus-button");
+const minusButton = plusMinusButton[0];
+const plusButton = plusMinusButton[1];
+
+const inputName = document.querySelector("#input-text");
+
+function generateTeams(event) {}
